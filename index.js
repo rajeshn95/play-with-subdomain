6789f0736e76397b0cc9b7a97e11aaa61b413b74
Rajesh Nautiyal <52703970+rajeshn95@users.noreply.github.com>
2024-05-26 13:29:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run "npm run dev" in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run "npm run deploy" to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
 
-async function handleRequest(request) {
-  const url = new URL(request.url)
-  const subdomain = url.hostname.split('.')[0]
+export default {
+  
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url)
+    const subdomain = url.hostname.split('.')[0]
+    const html = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Username</title>
+          <style>
+              body {
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                  height: 100vh;
+                  margin: 0;
+                  font-family: Arial, sans-serif;
+                  background-color: #f0f0f0;
+              }
+              h1 {
+                  font-size: 3rem;
+                  color: #333;
+              }
+          </style>
+      </head>
+      <body>
+          <h1>${subdomain}</h1>
+      </body>
+      </html>
+      `;
 
-  const html = `
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Username</title>
-      <style>
-          body {
-              display: flex;
-              justify-content: center;
-              align-items: center;
-              height: 100vh;
-              margin: 0;
-              font-family: Arial, sans-serif;
-              background-color: #f0f0f0;
-          }
-          h1 {
-              font-size: 3rem;
-              color: #333;
-          }
-      </style>
-  </head>
-  <body>
-      <h1>${subdomain}</h1>
-  </body>
-  </html>
-  `
-
-  return new Response(html, {
+    return new Response(html, {
     headers: { 'content-type': 'text/html;charset=UTF-8' },
   })
-}
+  },
+};
